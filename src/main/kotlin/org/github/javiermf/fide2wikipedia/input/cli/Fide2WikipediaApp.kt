153e623e14009a8403f82059bfef425a62746a0c
JavierMF <javier.munoz@gmail.com>
2025-09-11 08:39:45 +0200

    Generated file name with date pattern.

--- src/main/kotlin/org/github/javiermf/fide2wikipedia/input/cli/Fide2WikipediaApp.kt
+++ src/main/kotlin/org/github/javiermf/fide2wikipedia/input/cli/Fide2WikipediaApp.kt
@@ -5,15 +5,20 @@ import org.github.javiermf.fide2wikipedia.infrastructure.downloader.FideFilesDow
 import org.github.javiermf.fide2wikipedia.infrastructure.filewriter.OutputLocalFileWriter
 import java.io.File
 import java.time.Clock
+import java.time.format.DateTimeFormatter
 
 fun main() {
 
     val downloader = FideFilesDownloader()
     val clock = Clock.systemDefaultZone()
 
+    val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val currentDate = dateFormatter.format(clock.instant().atZone(clock.zone))
+    val outputFileName = "output-$currentDate.txt"
+
     GenerateWikipediaTableFromFideRatingsUseCase(
         downloader = downloader,
-        outputWriter = OutputLocalFileWriter(File("output.txt")),
+        outputWriter = OutputLocalFileWriter(File(outputFileName)),
         clock = clock
     ).generate()
 
